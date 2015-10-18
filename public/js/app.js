d529f2b4b9c21e1004365dc07370230cf73ee878
Andrew Betts <andrew.betts@ft.com>
2015-10-18 17:52:28 +0100

    I heard react was good

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,27 @@
 var Attendee = React.createClass({
 	handlePrintBadge: function(e) {
-		console.log('Print badge for', this.props.id);
+
+		// How to indicate activity?
+		//button.classList.add('activity');
+
+		fetch("/print/"+this.props.id, {method:'post'})
+			.then(function(responseStream) { return responseStream.json(); })
+			.then(function(result) {
+				if (result === true) {
+					button.classList.remove('activity');
+				} else {
+					console.log(result);
+				}
+			})
+		;
 	},
 	render: function() {
-		var checkInStatus = this.props.checkedIn ? 'checked-in' : '';
+		var attendeeClass = 'attendee ' + (this.props.checkedIn ? 'checked-in' : '');
 		return (
-			<li id='attendee-{this.props.id}' className='attendee {checkInStatus}'>
+			<li className={attendeeClass}>
 				<span className="icon-checkmark"></span>
 				<strong>{this.props.familyName}</strong>, {this.props.givenName}
-				<span className='btn icon-print' onClick={this.handlePrintBadge}></span>
+				<button className='btn icon-print' onClick={this.handlePrintBadge}>Print</button>
 			</li>
 		)
 	}
@@ -18,75 +31,75 @@ var AttendeeList = React.createClass({
 	render: function() {
 		var rows = [];
         this.props.attendees.forEach(function(attendee) {
-        	var sortKey = (attendee.checkedIn ? '0' : '1') + attendee.familyName;
             rows.push(<Attendee
             	familyName={attendee.familyName}
             	givenName={attendee.givenName}
             	id={attendee.id}
             	checkedIn={attendee.checkedIn}
-            	key={sortKey} />);
+            	key={attendee.id} />);
+        });
+        rows.sort(function(a, b) {
+        	return (a.checkedIn+a.familyName > b.checkedIn+b.familyName) ? 1 : -1;
         });
 		return (
 			<ul className='attendees-list'>
-			{rows}
+					{rows}
 			</ul>
 		);
 	}
 })
 
 var EventDashboard = React.createClass({
+	getInitialState: function() {
+
+		// this.state is immutable, which prevents modifying the results of Array.find in the checkin listener
+		this._mutableState = {attendees:[]};
+
+		// Need to include a skeleton to avoid treating undefined as an array in AttendeeList
+		return {attendees:[]};
+	},
+	componentDidMount: function() {
+		var source = new EventSource("stream");
+
+		fetch('/data')
+			.then(function(responseStream) { return responseStream.json(); })
+			.then(function(data) {
+				this.setState(data);
+				this._mutableState = data;
+			}.bind(this))
+			.catch(function(e) { console.log(e); })
+		;
+
+		source.addEventListener("checkin", function(e) {
+			var attendeeID = e.data;
+			var attendee = this._mutableState.attendees.find(function(attendee) {
+				return attendee.id === attendeeID;
+			});
+			attendee.checkedIn = true;
+			this.setState(this._mutableState);
+		}.bind(this), false);
+
+		source.addEventListener("uncheckin", function(e) {
+			var attendeeID = e.data;
+			var attendee = this._mutableState.attendees.find(function(attendee) {
+				return attendee.id === attendeeID;
+			});
+			attendee.checkedIn = false;
+			this.setState(this._mutableState);
+		}.bind(this), false);
+
+	},
 	render: function() {
 		return (
 			<div className='dashboard'>
-				<h1>{this.props.data.eventName} <date>{this.props.data.eventDate}</date></h1>
-				<AttendeeList attendees={this.props.data.attendees} />
+				<h1>{this.state.eventName} <date>{this.state.eventDate}</date></h1>
+				<AttendeeList attendees={this.state.attendees} />
 			</div>
 		);
 	}
 });
 
-var data = {
-	eventName: "London Web Performance",
-	eventDate: "2015-11-04T19:00:00Z",
-	attendees: [
-		{id:1, givenName:'Andrew', familyName:'Betts', checkedIn:true},
-		{id:2, givenName:'Perry', familyName:'Dyball', checkedIn:false},
-		{id:3, givenName:'Matt', familyName:'Andrews', checkedIn:false},
-		{id:4, givenName:'Alberto', familyName:'Alias', checkedIn:true}
-	]
-};
-
 ReactDOM.render(
-    <EventDashboard data={data} />,
+    <EventDashboard />,
     document.getElementById('container')
 );
-// ReactMagicMove
-
-/*
-var source = new EventSource("stream");
-
-source.addEventListener("checkin", function(e) {
-	var attendeeID = e.data;
-	document.getElementById('attendee-'+attendeeID).classList.add('checked-in');
-}, false);
-
-source.addEventListener("uncheckin", function(e) {
-	var attendeeID = e.data;
-	document.getElementById('attendee-'+attendeeID).classList.remove('checked-in');
-}, false);
-
-document.addEventListener('click', function(e) {
-	if (e.target.matches('.print-btn')) {
-		var button = e.target;
-		e.preventDefault();
-		button.classList.add('activity');
-		$.post(button.dataset.href, function(result) {
-			if (result === true) {
-				button.classList.remove('activity');
-			} else {
-				console.log(result);
-			}
-		});
-	}
-});
-*/
