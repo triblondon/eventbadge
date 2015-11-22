f888098d43aa010f533be2f43bb7b53f96ec29e4
Andrew Betts <andrew.betts@ft.com>
2015-11-22 11:50:22 +0900

    Name board

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,48 @@
 
-var source = new EventSource("stream");
+var source = new EventSource("/stream");
+var layoutTimer;
+var cont;
+
+function layout() {
+	cont.classList.remove('rendered');
+	clearTimeout(layoutTimer);
+	layoutTimer = setTimeout(function() {
+
+		var itemCount = document.querySelectorAll('.attendee').length;
+		var contWidth = cont.offsetWidth;
+		var contHeight = cont.offsetHeight;
+		var idealRatio = 12;
+		var maxCols = 5;
+		var minItemHeight = 30;
+		var minItemWidth = 200;
+		var fontSizeRatio = 2.5;  // px of item height for each px of font size
+
+		var bestRatio = 0, thisRatio = 0;
+		var bestColCount = 0, thisColCount = 0;
+		var thisItemHeight, thisItemWidth, bestItemHeight, bestItemWidth;
+		for (thisColCount=1; thisColCount<=maxCols; thisColCount++) {
+			thisItemWidth = contWidth / thisColCount;
+			thisItemHeight = contHeight / Math.ceil(itemCount/thisColCount);
+			thisRatio = thisItemWidth / thisItemHeight;
+			if (Math.abs(idealRatio - thisRatio) < Math.abs(idealRatio - bestRatio) && thisItemWidth > minItemWidth) {
+				console.log('Possible', thisColCount, 'cols');
+				bestRatio = thisRatio;
+				bestColCount = thisColCount;
+				bestItemWidth = thisItemWidth;
+				bestItemHeight = thisItemHeight;
+			}
+		}
+
+		cont.style.fontSize = (bestItemHeight / fontSizeRatio) + 'px';
+
+		console.log('Best', bestColCount);
+		if (!cont.classList.contains('cols-'+bestColCount)) {
+			cont.classList.remove('cols-1','cols-2','cols-3','cols-4','cols-5');
+			cont.classList.add('cols-'+bestColCount);
+		}
+		cont.classList.add('rendered');
+	}, 250);
+}
 
 source.addEventListener("checkin", function(e) {
 	var attendeeID = e.data;
@@ -16,7 +59,10 @@ document.addEventListener('click', function(e) {
 		var button = e.target;
 		e.preventDefault();
 		button.classList.add('activity');
-		$.post(button.dataset.href, function(result) {
+
+		fetch(button.dataset.href, {method:'post'})
+		.then(function(responseStream) { return responseStream.json(); })
+		.then(function(result) {
 			if (result === true) {
 				button.classList.remove('activity');
 			} else {
@@ -25,3 +71,9 @@ document.addEventListener('click', function(e) {
 		});
 	}
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	cont = document.querySelector('.attendees-list');
+	layout();
+});
+window.addEventListener('resize', layout);
